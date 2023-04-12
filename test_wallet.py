e8e20e9e775c76bf14f996753ba523dfdcf7c400
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-12 07:41:55 -0300

    carteira()

--- test_wallet.py
+++ test_wallet.py
@@ -2,17 +2,20 @@ import pytest
 from wallet import Wallet
 
 
+@pytest.fixture
+def carteira():
+    return Wallet(10)
+
+
 def test_saldo_inicial_sem_parametro():
     carteira = Wallet()
     assert carteira.saldo == 0
 
 
-def test_saldo_inicial_com_parametro():
-    carteira = Wallet(10)
+def test_saldo_inicial_com_parametro(carteira):
     assert carteira.saldo == 10
 
 
-def test_adiciona_saldo():
-    carteira = Wallet(10)
+def test_adiciona_saldo(carteira):
     carteira.add_cash(10)
     assert carteira.saldo == 20
