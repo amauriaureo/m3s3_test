dd3b8696ddae6fb14e322561804a121c68d604d5
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-14 08:40:28 -0300

    classe teste saldo inicial

--- test_wallet.py
+++ test_wallet.py
@@ -7,13 +7,15 @@ def carteira():
     return Wallet(10)
 
 
-def test_saldo_inicial_sem_parametro():
-    carteira = Wallet()
-    assert carteira.saldo == 0
+class TestSaldoInicial:
+
+    def test_saldo_inicial_sem_parametro(self):
+        carteira = Wallet()
+        assert carteira.saldo == 0
 
 
-def test_saldo_inicial_com_parametro(carteira):
-    assert carteira.saldo == 10
+    def test_saldo_inicial_com_parametro(self, carteira):
+        assert carteira.saldo == 10
 
 
 def test_adiciona_saldo(carteira):
