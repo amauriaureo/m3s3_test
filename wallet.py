b30756b4aa43c7ca179bffce408ce9a8ac88926d
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-14 08:40:07 -0300

    append

--- wallet.py
+++ wallet.py
@@ -6,17 +6,17 @@ class Wallet(object):
 
      def __init__(self, quantidade_inicial=0):
           self.saldo = quantidade_inicial
-          self.auditoria = []
+          self.auditoria = list()
 
 
      def spend_cash(self, quantidade):
           if self.saldo < quantidade:
-               self.auditoria.add(f"suspend spend {quantidade}")
+               self.auditoria.append(f"suspend spend {quantidade}")
                raise InsufficientAmount(f'Não há o suficiente disponível para gastar {quantidade}')
-          self.auditoria.add(f"spend {quantidade}")
+          self.auditoria.append(f"spend {quantidade}")
           self.saldo -= quantidade
 
 
      def add_cash(self, quantidade):
-          self.auditoria.add(f"Add {quantidade}")
+          self.auditoria.append(f"Add {quantidade}")
           self.saldo += quantidade
