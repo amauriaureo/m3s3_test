5c202b10c51cd51a3c2545d332e2102efa484e32
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-14 08:43:04 -0300

    teste retira saldo class

--- test_wallet.py
+++ test_wallet.py
@@ -22,18 +22,17 @@ class TestAdicionaSaldo:
         assert carteira.saldo == 20
 
 
-def test_retirar_da_carteira_com_saldo(carteira):
-    carteira.spend_cash(10)
-    assert carteira.saldo == 0
+class TestRetiraSaldo:
+    def test_retirar_da_carteira_com_saldo(self, carteira):
+        carteira.spend_cash(10)
+        assert carteira.saldo == 0
 
+    def test_retirar_da_carteira_sem_saldo_suficiente(self, arteira):
+        # carteira.spend_cash(10)
+        # assert carteira.saldo == 0
+        with pytest.raises(InsufficientAmount):
+            carteira.spend_cash(20)
 
-def test_retirar_da_carteira_sem_saldo_suficiente(carteira):
-    # carteira.spend_cash(10)
-    # assert carteira.saldo == 0
-    with pytest.raises(InsufficientAmount):
-        carteira.spend_cash(20)
-
-
-def test_retira_da_carteira_parte_do_saldo(carteira):
-    carteira.spend_cash(5)
-    assert carteira.saldo == 5
+    def test_retira_da_carteira_parte_do_saldo(self, carteira):
+        carteira.spend_cash(5)
+        assert carteira.saldo == 5
