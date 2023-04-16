28822fa31617e14bd20e6d81bc76f03ffccbf902
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-16 13:48:36 -0300

    paciente.setter

--- paciente/paciente.py
+++ paciente/paciente.py
@@ -10,3 +10,11 @@ class Paciente:
     @property
     def paciente(self):
         return self._paciente
+
+    @paciente.setter
+    def paciente(self, nome):
+        if not isinstance(nome, str):
+            raise TypeError("'nome' inválido")
+        if nome == '':
+            raise NameIsEmptyError("'nome' é obrigatório")
+        self._paciente = nome
