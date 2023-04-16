0ea00c3e9047373cf7b84514b5a2d433125b23d6
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-16 13:51:37 -0300

    except TypeError

--- paciente/vet_app.py
+++ paciente/vet_app.py
@@ -3,3 +3,5 @@ from paciente import Paciente, NameIsEmptyError
 try:
     nome = input('Digite o nome do paciente: ')
     p = Paciente(nome)
+except TypeError:
+    print('O nome deve ser uma string. Tente novamente...')
