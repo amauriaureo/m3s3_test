0a912506928ffcb3704cf0a776829f2ca6694044
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-16 13:53:28 -0300

    except Exception

--- paciente/vet_app.py
+++ paciente/vet_app.py
@@ -7,3 +7,6 @@ except TypeError:
     print('O nome deve ser uma string. Tente novamente...')
 except NameIsEmptyError:
     print('O nome não poder ser uma string vazia. Preencha uma valor...')
+except Exception as e:
+    print('Ocorreu um erro inesperado ao criar o objeto de Paciente.')
+    print('informações do erro: ', e)
