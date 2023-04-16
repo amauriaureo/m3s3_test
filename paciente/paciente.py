fb47fcf226cf47e3429c325b30244085de65043a
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-16 13:48:16 -0300

    property

--- paciente/paciente.py
+++ paciente/paciente.py
@@ -1,7 +1,12 @@
 class NameIsEmptyError(Exception):
     pass
 
+
 class Paciente:
     def __init__(self, nome):
         self.paciente = nome
         # restante do código que inicializa os dados do paciente.
+
+    @property
+    def paciente(self):
+        return self._paciente
