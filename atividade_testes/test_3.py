96b0b3908f2dcebe10628fca8e67246e7e81e1db
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-20 08:40:05 -0300

    import

---
+++ atividade_testes/test_3.py
@@ -0,0 +1,3 @@
+import pytest
+from exercicio3 import ler_dimensoes_objeto, calcular_preco_volume, validar_medida, ler_peso_objeto, calcular_multiplicador_peso, ler_rota, calcular_multiplicador_rota, calcular_frete, gerar_codigo, cadastrar_peca, imprimir_peca, consultar_pecas, remover_peca
+
