a3b2de8df6f96ab70b02500cf617f43d5d66d355
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-20 08:40:45 -0300

    fixture

--- atividade_testes/test_3.py
+++ atividade_testes/test_3.py
@@ -1,3 +1,8 @@
 import pytest
 from exercicio3 import ler_dimensoes_objeto, calcular_preco_volume, validar_medida, ler_peso_objeto, calcular_multiplicador_peso, ler_rota, calcular_multiplicador_rota, calcular_frete, gerar_codigo, cadastrar_peca, imprimir_peca, consultar_pecas, remover_peca
 
+
+@pytest.fixture
+def dimensoes():
+    ler_dimensoes_objeto()
+
